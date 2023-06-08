df60aeb20e5259609748ce455e8fe85b63673ee6
lemisora <lemisorae@protonmail.com>
2023-06-07 18:33:43 -0600

    Se modifica la clase Nivel y se vuelve de tipo abstract para polimorfismo

---
+++ Nivel/Nivel.java
@@ -0,0 +1,8 @@
+package Nivel;
+abstract public class Nivel{
+  public Nivel(){} //Constructor de la clase Nivel
+  public boolean sePuedeEsquivar;
+  public int balasRestantes;
+
+  abstract public void esquivarObjeto();
+}
