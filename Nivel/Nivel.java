5d1b522770617caf21a374e733aa9587c7355592
lemisora <leonardo.michel.dom@gmail.com>
2023-07-31 01:00:03 +0000

    Reinicio de la estructura del proyecto y de la jerarquía de clases

--- Nivel/Nivel.java
+++
@@ -1,8 +0,0 @@
-package Nivel;
-abstract public class Nivel{
-  public Nivel(){} //Constructor de la clase Nivel
-  public boolean sePuedeEsquivar;
-  public int balasRestantes;
-
-  abstract public void esquivarObjeto();
-}
