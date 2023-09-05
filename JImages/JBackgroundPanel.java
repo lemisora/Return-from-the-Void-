4c96946e55ee8ca8553718302adf9e3978d705d6
unknown <alejanafs28@gmail.com>
2023-09-04 18:53:49 -0600

    Se agregao el background al menu y los botones cambian de nivel

--- JImages/JBackgroundPanel.java
+++ JImages/JBackgroundPanel.java
@@ -9,23 +9,15 @@ import javax.imageio.ImageIO;
 
 public class JBackgroundPanel extends JPanel {
     private BufferedImage visible;
-    private BufferedImage[] bufImage;
 
     public JBackgroundPanel() {
-        this.bufImage = new BufferedImage[6];
-        for(int i=0; i<6; i++){
-            try {
-                this.bufImage[i] = ImageIO.read(new File("Resource/" + (i+1)+ ".png"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try {
+            this.visible = ImageIO.read(new File("Resource/Images/7.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        this.visible = this.bufImage[5];
     }
 
-    public void setImagen(int index) {
-        this.visible = bufImage[index];
-    }
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -36,20 +28,8 @@ public class JBackgroundPanel extends JPanel {
         }
     }
 
-    public void changeBackground(int index) {
-        System.out.print("hola");
-        this.setImagen(index);
-        this.repaint();
-    }
 
-    public void changeWindow(JFrame frame, int index){
-        this.changeBackground(index);
-        frame.getContentPane().removeAll();
-
-        frame.repaint();
-        //Se podría modificar para poder añadir el panel (Canvas) de cada nivel aquí mismo y añadir el inicio del hilo del nivel 1 acá por ejemplo
-    }
-
+   
     public void addMainComponents(int WIDTH, int HEIGHT, JFrame frame){
 
         this.setSize(WIDTH, HEIGHT);
@@ -58,24 +38,18 @@ public class JBackgroundPanel extends JPanel {
         JButton starLv1 = new JButton();
         JButton starLv2 = new JButton();
         JButton starLv3 = new JButton();
-        JButton scoreB = new JButton("S");
-        JButton creditsB = new JButton("!");
-        JButton exiButton = new JButton("X");
 
         starLv1.setOpaque(true);
         starLv1.setContentAreaFilled(false);
         starLv1.setBorderPainted(false);
-        starLv1.addActionListener(e -> this.changeWindow(frame, 0));
 
         starLv2.setOpaque(true);
         starLv2.setContentAreaFilled(false);
         starLv2.setBorderPainted(false);
-        starLv2.addActionListener(e -> this.changeBackground(1));
 
         starLv3.setOpaque(true);
         starLv3.setContentAreaFilled(false);
         starLv3.setBorderPainted(false);
-        starLv3.addActionListener(e -> this.changeBackground(2));
         
         starLv1.setBounds(313, 190, 361, 65);
         starLv2.setBounds(313, 285, 361, 65);
--- Menu.java
+++ Menu.java
@@ -1,13 +1,13 @@
+import java.awt.image.BufferedImage;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-import Model.Loader;
+import JImages.*;
 
 public class Menu extends JFrame{
     private final int WIDTH=1000, HEIGHT=600;
-    JPanel panel;
+    JBackgroundPanel panel;
     JButton lev1B, lev2B, lev3B;
+    BufferedImage backGroundPanel;
 
     public Menu(){
         setTitle("Return from the Void!");
@@ -16,31 +16,60 @@ public class Menu extends JFrame{
         setResizable(false);
         setLocationRelativeTo(null);
 
-        panel = new JPanel();
+        //Creacion del panel
+        panel = new JBackgroundPanel();
         panel.setLayout(null);
 
         //Creacion de los botones
-        lev1B = new JButton("Level 1");
+        lev1B = new JButton();
+        lev1B.setOpaque(true);
+        lev1B.setContentAreaFilled(false);
+        lev1B.setBorderPainted(false);
+
+        lev2B = new JButton();
+        lev2B.setOpaque(true);
+        lev2B.setContentAreaFilled(false);
+        lev2B.setBorderPainted(false);
+
+        lev3B = new JButton();
+        lev3B.setOpaque(true);
+        lev3B.setContentAreaFilled(false);
+        lev3B.setBorderPainted(false);
 
         //Agregamos action listener a los botones
         lev1B.addActionListener(e-> goToLevel(1));
+        lev2B.addActionListener(e-> goToLevel(2));
+        lev3B.addActionListener(e-> goToLevel(3));
 
         //Posicionamos los botones
-        lev1B.setBounds(450,100, 100, 30);
+        lev1B.setBounds(300, 190, 390, 70);
+        lev2B.setBounds(300, 300, 390, 70);
+        lev3B.setBounds(300, 410, 390, 70);
 
         //Agregamos los botones al panel
         panel.add(lev1B);
+        panel.add(lev2B);
+        panel.add(lev3B);
 
         add(panel);
         setVisible(true);
     }
 
+    //Funcion para cambiar de nivel
     private void goToLevel(int index){
         this.setVisible(false);
         if(index==1){
             Nivel1 lvl1 = new Nivel1();
             lvl1.start();
             lvl1.setVisible(true);
+        }
+        if(index == 2){
+            System.out.println("ir al nivel 2");
+            this.setVisible(true);
+        }
+        if(index == 3){
+            System.out.println("ir al nivel 3");
+            this.setVisible(true);
         }
     }
 
